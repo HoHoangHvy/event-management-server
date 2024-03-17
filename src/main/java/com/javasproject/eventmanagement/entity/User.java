581d2ee50c97d80a67ac1e87c7dbdcaa422cf8e5
Hồ Hoàng Huy <127609002+HoHoangHvy@users.noreply.github.com>
2024-03-17 18:12:54 +0700

    Users [UPDA] : Refactor user class

--- src/main/java/com/javasproject/eventmanagement/entity/User.java
+++ src/main/java/com/javasproject/eventmanagement/entity/User.java
@@ -1,75 +1,26 @@
 package com.javasproject.eventmanagement.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    private String userName;
-    private String password;
-    private String firstName;
-    private String lastName;
-    private LocalDate dateOfBirth;
-    private String status;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String user_name) {
-        this.userName = user_name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String first_name) {
-        this.firstName = first_name;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String last_name) {
-        this.lastName = last_name;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate date_of_birth) {
-        this.dateOfBirth = date_of_birth;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    String id;
+    String userName;
+    String password;
+    String firstName;
+    String lastName;
+    LocalDate dateOfBirth;
+    String status;
 }
